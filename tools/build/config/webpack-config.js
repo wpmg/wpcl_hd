9ba60848229f26c89e9c8cdb35b7f31545da8076
wpmg <wilmer.prentius@gmail.com>
2017-07-11 14:08:56 +0200

    use no externals

--- tools/build/config/webpack-config.js
+++ tools/build/config/webpack-config.js
@@ -15,15 +15,15 @@ const BUILD_FILE = '[name].js';
 const BUILD_FOLDER = PATH.resolve(__dirname, ROOT, 'app/public/js/');
 const PUBLIC_PATH = '/js/';
 
-var webpackConfig = {
+const webpackConfig = {
   entry: {
-    'app_public': PUBLIC_ENTRY_FILE,
-    'app_dashboard': DASHBOARD_ENTRY_FILE
+    app_public: PUBLIC_ENTRY_FILE,
+    app_dashboard: DASHBOARD_ENTRY_FILE,
   },
   output: {
     path: BUILD_FOLDER,
     publicPath: PUBLIC_PATH,
-    filename: BUILD_FILE
+    filename: BUILD_FILE,
   },
   devtool: 'inline-source-map',
   bail: true,
@@ -32,37 +32,23 @@ var webpackConfig = {
       {
         test: /\.js$/,
         include: [
-          APP_FOLDER
+          APP_FOLDER,
         ],
         loader: 'babel-loader',
         query: {
           compact: false,
           cacheDirectory: true,
-          presets: ['react']
-        }
-      }
-    ]
-  },
-  externals: {
-    'axios': 'axios',
-    'react': 'React',
-    'react-router': 'ReactRouter',
-    'history': 'History',
-    'react-dom': 'ReactDOM'
+          presets: ['react'],
+        },
+      },
+    ],
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new webpack.ProvidePlugin({
-      bootstrap: 'bootstrap.scss'
-    }),
-    new webpack.ProvidePlugin({
-      d3: 'd3'
-    })
-  ]
+  ],
 };
 
 module.exports = webpackConfig;
